c12b36031498043007942d4184f6198753c4bb24
abhishek-goutam <abhishekgoutam8894@gmail.com>
2023-04-13 16:59:34 +0530

    task 17

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,12 +2,12 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/product-list", {
-        prods: rows,
+        prods: products,
         path: "/All Products",
-        pageTitle: "Shop",
+        pageTitle: "/products",
       });
     })
     .catch((err) => console.log(err));
@@ -15,22 +15,32 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
-      res.render("shop/product-detail", {
-        product: product[0],
-        pageTitle: product.title,
-        path: "/products",
-      });
-    })
-    .catch((err) => console.log(err));
+  console.log("prodId-----",req.params)
+  Product.findAll({where:{id:prodId}}).then(products=>{
+    res.render("shop/product-detail", {
+      product: products[0],
+      pageTitle: products[0].title,
+      path: "/products",
+    });
+  }
+  ).catch(err=>console.log(err));
+  // Product.findByPk(prodId)
+  //   .then(product => {
+  //     console.log("productssssss",product)
+  //     res.render("shop/product-detail", {
+  //       product: product,
+  //       pageTitle: product.title,
+  //       path: "/products",
+  //     });
+  //   })
+  //   .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/index", {
-        prods: rows,
+        prods: products,
         path: "/",
         pageTitle: "Shop",
       });
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'abhishek',
-    database:'db_connect',
-    password:'password'
+const sequelize = new Sequelize("db_connect", "abhishek", "password", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports= pool.promise();
+module.exports = sequelize; 
